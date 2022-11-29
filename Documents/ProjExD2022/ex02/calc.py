666b6ea700f3766fc2354e25f72f18ca21667a6f
c0a21061 <c0a21061dd@edu.teu.ac.jp>
2022-11-29 14:48:45 +0900

    あ

--- Documents/ProjExD2022/ex02/calc.py
+++ Documents/ProjExD2022/ex02/calc.py
@@ -5,18 +5,22 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     #tkm.showinfo("", f"{num}ボタンがクリックされました")
-    entry.insert(tk.END, num)
+    #entry.insert(tk.END, num)
+    if num == "=":
+        pass
+    else:
+        entry.insert(tk.insert(tk.END, num))
 
 root = tk.Tk()
 root.geometry("300x500")
 
 entry = tk.Entry(root, justify="right", width=10, font=("", 40))
-entry.grid(row=0, colum=0, columspan=3)
+entry.grid(row=0, column=0, columnspan=3)
 
 r, c = 1, 0
 for num in range(9, -1, -1):
-    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30).pack())
-    button.grid(row=r, colum=c)
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
     button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
@@ -26,7 +30,8 @@ for num in range(9, -1, -1):
 operators = ["+", "="]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, fon=("", 30))
-    button.grid(row=r, colum=c)
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
