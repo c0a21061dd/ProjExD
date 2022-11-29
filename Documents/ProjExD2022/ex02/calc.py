8478944f5c6d3a912e48344d43e2c9d58a137536
c0a21061 <c0a21061dd@edu.teu.ac.jp>
2022-11-29 14:45:20 +0900

    あ

---
+++ Documents/ProjExD2022/ex02/calc.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    #tkm.showinfo("", f"{num}ボタンがクリックされました")
+    entry.insert(tk.END, num)
+
+root = tk.Tk()
+root.geometry("300x500")
+
+entry = tk.Entry(root, justify="right", width=10, font=("", 40))
+entry.grid(row=0, colum=0, columspan=3)
+
+r, c = 1, 0
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30).pack())
+    button.grid(row=r, colum=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
+operators = ["+", "="]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, fon=("", 30))
+    button.grid(row=r, colum=c)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
+root.mainloop()
