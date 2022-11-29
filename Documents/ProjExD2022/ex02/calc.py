585762e03c5ee90b51301a1097f2734b23f1a756
c0a21061 <c0a21061dd@edu.teu.ac.jp>
2022-11-29 17:21:18 +0900

    訂正しました #6

--- Documents/ProjExD2022/ex02/calc.py
+++ Documents/ProjExD2022/ex02/calc.py
@@ -25,44 +25,18 @@ entry = tk.Entry(root, justify="right", width=10, font=("", 30))
 entry.grid(row=0, column=0, columnspan=3)
 
 r, c = 1, 0
-operators = ["+", "-", "*", "/"]
-operators_add = ["**0.5", "**2", "="]
+#for文で全部回してみました！！
+operators = ["+", "-", "*", "/", "7", "8", "9", "**0.5", "4", "5", "6", "**2", "3", "2", "1", "=", "c", "0", "."]
 
+#表の(1,0)から右へ進み、四つ目のボタンまで届けば、下の段へ移行します
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 20))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
     c += 1
-
-r += 1
-for sign in operators_add:
-    button = tk.Button(root, text=f"{sign}", width=4, height=2, font=("", 20))
-    button.grid(row=r, column=3)
-    button.bind("<1>", button_click)
-    operators_add
-    r += 1
-
-r, c = 4, 0
-for num in range(1, 10, +1):
-    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 20))
-    button.grid(row=r, column=c)
-    button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
-        r -= 1
-        c = 0
-
-button = tk.Button(root, text="c", width=4, height=2, font=("", 20))
-button.grid(row=5, column=0)
-button.bind("<1>", button_click)
-
-button = tk.Button(root, text="0", width=4, height=2, font=("", 20))
-button.grid(row=5, column=1)
-button.bind("<1>", button_click)
-
-button = tk.Button(root, text=".", width=4, height=2, font=("", 20))
-button.grid(row=5, column=2)
-button.bind("<1>", button_click)
+    if c == 4:
+        r += 1
+        c -= 4
 
 root.mainloop()
 
