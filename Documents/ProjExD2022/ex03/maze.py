b0f108bf9b99fbcd6099ff12644b39b0e4155d68
c0a21061 <c0a21061dd@edu.teu.ac.jp>
2022-12-06 15:26:19 +0900

    あ

--- Documents/ProjExD2022/ex03/maze.py
+++ Documents/ProjExD2022/ex03/maze.py
@@ -7,25 +7,35 @@ def key_down(event):
 def key_up(event):
     global key
     key = ""
+
+
 def main_proc():
-    global cx, cy
-    if key == "Up": cy -= 20
-    if key == "Down": cy += 20
-    if key == "Left": cx -= 20
-    if key == "Right": cx += 20
+    global cx, cy, mx, my
+    if key == "Up": my -= 1
+    if key == "Down": my += 1
+    if key == "Left": mx -= 1
+    if key == "Right": mx += 1
+    if maze_lst[mx][my] == 1: # 移動先が壁だったら
+        if key == "Up": my += 1
+        if key == "Down": my -= 1
+        if key == "Left": mx += 1
+        if key == "Right": mx -= 1        
+    cx, cy = mx*100+50, my*100+50
     canvas.coords("kokaton", cx, cy)
     root.after(100, main_proc)
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
-
     maze_lst = mm.make_maze(15, 9)
     # print(maze_lst)
     mm.show_maze(canvas, maze_lst)
 
     cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     tori = tk.PhotoImage(file="fig/8.png")
     canvas.create_image(cx, cy, image=tori, tag="kokaton")
     key = ""
